07375672117c4a6c1129e95f9da942918289fe08
DrDaDoritos <daniyar.sailaubaev25@fizmat.kz>
2026-04-28 09:06:22 +0500

    Add Practice6 - file handling and built-in functions examples

--- TSIS1/connect.py
+++ TSIS1/connect.py
@@ -4,7 +4,7 @@ def get_connection():
     return psycopg2.connect(
         dbname="phonebook",
         user="postgres",
-        password="1234",
+        password="Amikosha2007",
         host="localhost",
         port="5432"
     )
